11b02720ae6f8a16d67d0374d1206f0f65963a42
nahidhk <nahidappltd@gmail.com>
2025-10-02 00:52:30 +0600

    Bug: I've been trying for 7 hours but can't fix this bug (+_+)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import githubApi from "../data/GithubApi";
+import usererrorimg from "../assets/user_error.webp"
 
 function Home() {
   const [userData, setUserData] = useState(null)
@@ -13,15 +14,51 @@ function Home() {
   return (
     <div>
       <div className="flex">
-        <div>
-          <img src={userData?.avatar_url} alt="" className="userimg cercel border" id="randomBorder" />
-          <h1 className="userName">{userData?.name}</h1>
-          <h3 className="userBio">{userData?.bio}</h3>
-          <h3 className="userLocation">{userData?.location}</h3>
-          <h3 className="userLink"><a href={userData?.html_url} target="_blank" rel="noreferrer">{userData?.html_url}</a></h3>
+        <div className="w300px">
+          {
+            userData?.avatar_url === null ?
+              <img className="userimg cercel border" src={usererrorimg} alt="Avater" />
+              :
+
+              <img className="userimg cercel border" src={userData?.avatar_url} alt="Avater" />
+          }
+          <br />
+          {
+            userData?.name === null ?
+              " "
+              :
+              <span className="title">
+                {userData?.name}
+                <br />
+                <i class="fa-brands fa-github big-x"></i>(
+                {
+                  <a href={userData?.html_url}>{userData?.login}</a>
+                }
+                )
+              </span>
+          }
+          <br />
+          <br />
+          {
+            <p>
+              {
+                userData?.bio
+              }
+            </p>
+          }
+          {
+            userData?.location === null ?
+            ""
+            :
+           <p>
+            <i class="fa-solid fa-location-dot big-xxl"></i>
+            {
+              userData?.location
+            }
+           </p>
+          }
         </div>
         <div className="overbox">
-
         </div>
       </div>
     </div>
